5c54041c6511955edda665c3be1274279613de82
Choisungbum <42492672+Choisungbum@users.noreply.github.com>
2023-09-14 17:34:23 +0900

    fix : 49191
    49191

--- src/codingTest/Level3_49191.java
+++ src/codingTest/Level3_49191.java
@@ -1,34 +1,47 @@
 package codingTest;
 
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.List;
 
 public class Level3_49191 {
-	static void dfs(int nodeIndex, boolean[] visited, int[][] copy) {
+	static int idx = 0;
+	static void dfs(int nodeIndex, boolean[] visited, int[][] copy, int[][] result) {
 		visited[nodeIndex] = true;
 		
-		System.out.println(nodeIndex + " -> " ); // 방문 노드 출력 
-		
-		for (int node : copy[nodeIndex]) {
+//		System.out.println(nodeIndex + " -> " ); // 방문 노드 출력 
+		for(int i = 0; i < copy[nodeIndex].length; i++)
 			if (!visited[nodeIndex]) {
-				dfs(nodeIndex, visited, copy);
+				int node = copy[nodeIndex][i];
+				result[idx][i] = node;
+				dfs(node, visited, copy, result);
 			}
-		}
 	}
 	
 	public int solution(int n, int[][] results) {
 		boolean[] visited = new boolean[results.length + 1]; // 방문처리 
 		
 		int[][] copy = new int[results.length + 1][];
+		int[][] result = new int[results.length + 1][n - 1];
 		copy[0] = new int[0];		// 빈 배열 추가 
+
 		System.out.println(copy[0].toString());
 		
 		for (int i = 1; i < results.length + 1; i++) {
 			copy[i] = Arrays.copyOf(results[i - 1], results[i - 1].length);
-			//System.out.println(copy[0][0]); -> null
 			System.out.println("copy[" + i + "]" + " " + copy[i][0]);
 		}
 		
-		dfs(1, visited, copy);
+		for (int[] arr :result) {
+			Arrays.fill(arr, -1);
+		}
+		for (int i = 1; i < results.length + 1; i++) {
+			dfs(i, visited, copy, result);
+		} // 
+		
+		for (int i = 1; i < results.length + 1; i++) {
+		}
+		
 		
         int answer = 0;
         return answer;
