23e92bf7b69b3f81a3cb60403845c6d6c9c4ca4c
Choisungbum <42492672+Choisungbum@users.noreply.github.com>
2023-09-15 17:52:03 +0900

    fix : 49191
    49191

--- src/codingTest/Level3_49191.java
+++ src/codingTest/Level3_49191.java
@@ -2,28 +2,58 @@ package codingTest;
 
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 public class Level3_49191 {
 	static int idx = 0;
-	static void dfs(int nodeIndex, boolean[] visited, int[][] copy, int[][] result) {
+	static List<Integer> dfs(int nodeIndex, boolean[] visited, Map<Integer, List<Integer>> graph, List<Integer> list) {
 		visited[nodeIndex] = true;
+		System.out.println(nodeIndex + " -> ");
+		List<Integer> node = graph.get(nodeIndex);
+//		List<Integer> list = new ArrayList<Integer>();
+		if (node == null) return list;
+		
+		for (int i : node) {
+//			list.add(nodeIndex);
+			if(!visited[i]) {
+				list.add(i);
+				dfs(i, visited, graph, list);
+				visited[i] = false;
+			}
+		}
+		System.out.println("end");
+		return list;
 	}
 	
 	public int solution(int n, int[][] results) {
 		boolean[] visited = new boolean[results.length + 1]; // 방문처리 
+		List<List<Integer>> answerlist = new ArrayList<>();
+		Map<Integer, List<Integer>> graph = new HashMap<>();
 		
-//		int[][] copy = new int[results.length + 1][];4
-		List<List<Integer>> ans = new ArrayList<>();
-		int[][] result = new int[results.length + 1][n];
-
-		for (int i = 0; i < results.length + 1; i++) {
-			ans.add(new ArrayList<>());
+		for (int i = 0; i < results.length; i++) {
+			if (graph.get(results[i][0]) == null) {
+				List<Integer> node = new ArrayList<>();
+				node.add(results[i][1]);
+				graph.put(results[i][0], node);
+			} else {
+				graph.get(results[i][0]).add(results[i][1]);
+			}
 		}
 		
-		for (int i = 1; i < results.length +1; i++) {
-			List<Integer> node = new ArrayList<>();
-			ans.get(results[i][0]).add(results[i][1]);
+		for (int i : graph.keySet()) {
+			System.out.println(i);
+		}
+		
+		for (List<Integer> list : graph.values()) {
+			System.out.println(list.toString());
+		}
+		for (int i : graph.keySet()) {
+			List<Integer> list = new ArrayList<Integer>();
+			list.add(i);
+			answerlist.add(dfs(i, visited, graph, list));
+			Arrays.fill(visited, false);
 		}
 		
         int answer = 0;
