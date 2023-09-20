90816e91a610dc23771a5e01b4efb9cd0bfe6c80
sungbumchoi <46594664+sungbumchoi@users.noreply.github.com>
2023-09-21 00:07:32 +0900

    fix : 49190 완료
    49190 완료

--- src/codingTest/Level5_49190.java
+++ src/codingTest/Level5_49190.java
@@ -1,11 +1,11 @@
 package codingTest;
 
 import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.HashSet;
-import java.util.List;
-import java.util.Map;
-import java.util.Set;
+	import java.util.HashMap;
+	import java.util.HashSet;
+	import java.util.List;
+	import java.util.Map;
+	import java.util.Set;
 
 public class Level5_49190 {
 	private static class Vertex {
@@ -27,17 +27,33 @@ public class Level5_49190 {
 	}
 	
 	private static final int[] dx = new int[] {0, 1, 1, 1, 0, -1, -1, -1}; // x 변화량
-	private static final int[] dy = new int[] {1, 1, 0, -1, -1, -1, 0, 1}; // y 변화량
+	private static final int[] dy = new int[] {-1, -1, 0, 1, 1, 1, 0, -1}; // y 변화량
 	
     public int solution(int[] arrows) {
     	int count = 0;
     	Map<String, Vertex> vertices = new HashMap<>();
     	
     	// 개수 세기
+    	Vertex v = new Vertex(0, 0);
+    	vertices.put(v.id, v);
+    	
     	for (int d : arrows) {
-    		int x = v.x + dx[d];
-    		int y = v.y + dy[d];
-    		String id = Vertex.id(x, y);
+    		for (int i = 0; i < 2; i++) {
+    			int x = v.x + dx[d];
+        		int y = v.y + dy[d];
+        		String id = Vertex.id(x, y);
+        		
+        		if (!vertices.containsKey(id)) {
+            		vertices.put(id, new Vertex(x, y));
+            	} else if (!v.connectedVertices.contains(id)){
+            		count++;
+            	}
+        		
+        		Vertex u = vertices.get(id);
+        		v.connectedVertices.add(u.id);
+        		u.connectedVertices.add(v.id);
+        		v = vertices.get(id);
+    		}
     	}
     	
     	return count;
