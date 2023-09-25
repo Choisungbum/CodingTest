faecf024ed945eb0978a7afe387552ece7869572
sungbumchoi <46594664+sungbumchoi@users.noreply.github.com>
2023-09-25 23:55:21 +0900

    feat : 42892 길 찾기 게임
    42892 길 찾기 게임

---
+++ src/codingTest/Level3_42892.java
@@ -0,0 +1,13 @@
+package codingTest;
+
+public class Level3_42892 {
+	 public int[][] solution(int[][] nodeinfo) {
+	        int[][] answer = {};
+	        return answer;
+	    }
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
