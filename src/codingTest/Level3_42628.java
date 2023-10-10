e1de64a6bbec4ff06acd26a271356abfbd0b58b9
sungbumchoi <46594664+sungbumchoi@users.noreply.github.com>
2023-10-10 22:21:50 +0900

    feat : 42628
    42628

---
+++ src/codingTest/Level3_42628.java
@@ -0,0 +1,21 @@
+package codingTest;
+
+import java.util.Collection;
+import java.util.Collections;
+import java.util.PriorityQueue;
+
+public class Level3_42628 {
+	public int[] solution(String[] operations) {
+		PriorityQueue<Integer> pq = new PriorityQueue<>();
+		pq.comparator();
+        int[] answer = {};
+        return answer;
+    }
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String[] operations = new String[] {"I 16", "I -5643", "D -1", "D 1", "D 1", "I 123", "D -1"};
+		Level3_42628 cls = new Level3_42628();
+		System.out.println(cls.solution(operations));
+	}
+
+}
