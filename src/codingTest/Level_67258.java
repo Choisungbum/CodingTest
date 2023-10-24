46f32936bb5039af5502e25fbf963c5386393466
Choisungbum <42492672+Choisungbum@users.noreply.github.com>
2023-10-24 17:37:19 +0900

    fix : 67258
    67258

--- src/codingTest/Level_67258.java
+++ src/codingTest/Level_67258.java
@@ -1,7 +1,9 @@
 package codingTest;
 
+import java.util.ArrayDeque;
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Deque;
 import java.util.HashSet;
 import java.util.LinkedList;
 import java.util.List;
@@ -15,19 +17,18 @@ public class Level_67258 {
 		Set<String> kind = new HashSet<>(Arrays.asList(gems));
 		if (kind.size() == 1) return result ;
 		// 1 에 gems 추가
-		Queue<String> q_gems = new LinkedList<>(Arrays.asList(gems));
+		Deque<String> dq_gems = new ArrayDeque<>();
 		List<String> list_gems = new ArrayList<>();
 		Set<String> set_gems = new HashSet<>();
 		// 2. q 에 있는
 		int start = 1;
 		int end = 1;
-		while (!q_gems.isEmpty()) {
-			// list_gem 에 기존 q에 들어있는 보석 후보 삽입
-			String gem = q_gems.poll();
-			list_gems.add(gem);
+		
+		for (int i = 0; i < gems.length; i++) {
+			dq_gems.add(gems[i]);
 			
 			// Set.copyOf 메서드를 통해 list에 있는 값이 kind의 갯수와 같을 경우 result 후보 
-			if (Set.copyOf(list_gems).size() == kind.size()) {
+			if (Set.copyOf(dq_gems).size() == kind.size()) {
 				if (result[1] == 1) {
 					result[0] = start;
 					result[1] = end;
@@ -36,25 +37,23 @@ public class Level_67258 {
 					result[1] = end;
 				}
 			}
-						
 			end += 1;
 			System.out.println("end : " + end);
 			
-			if (list_gems.size() > 1) {
-				if (list_gems.get(0).equals(gem)) {
-					list_gems.remove(0);
-					start += 1;
-					System.out.println("start : " + start);
-				}
+			while (dq_gems.size() > 1 && dq_gems.peekFirst().equals(dq_gems.peekLast())) {
+				dq_gems.pollFirst();
+				start += 1;
+				System.out.println("start : " + start);
 				
-				while (list_gems.get(0).equals(list_gems.get(1))) {
-					list_gems.remove(0);
+				String tmp = dq_gems.pollFirst();
+				
+				while (dq_gems.size() > 1 && tmp.equals(dq_gems.peekFirst())) {
 					start += 1;
 					System.out.println("start : " + start);
 				}
+				dq_gems.addFirst(tmp);
 			}
-			
- 		}
+		}
 		
         return result;
     }
