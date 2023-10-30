ac087ee8ef551c8232f64f232f0959d7e1fe8da0
sungbumchoi <46594664+sungbumchoi@users.noreply.github.com>
2023-10-30 22:17:49 +0900

    feat : 64063
     64063

---
+++ src/codingTest/Level4_64063.java
@@ -0,0 +1,13 @@
+package codingTest;
+
+public class Level4_64063 {
+    public long[] solution(long k, long[] room_number) {
+        long[] answer = {};
+        return answer;
+    }
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
