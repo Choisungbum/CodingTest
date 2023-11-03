1c98738a07ccc1b141c475419c08a2b46be437d4
Choisungbum <42492672+Choisungbum@users.noreply.github.com>
2023-11-03 15:56:29 +0900

    fix : 64063
    64063

--- src/codingTest/Level3_64063.java
+++ src/codingTest/Level3_64063.java
@@ -1,39 +1,81 @@
 package codingTest;
 
+import java.util.ArrayList;
 import java.util.HashMap;
-import java.util.HashSet;
+import java.util.List;
 import java.util.Map;
-import java.util.Set;
 
 public class Level3_64063 {
-	private static int[] parents;
-	private static int[] rn;
-	
-	private int find(int x) {
-		if (parents[x] == x) return x;
-		return find(parents[x]);
-	}
-	
-	private boolean union(int x, int y) {
-		int ele = find(x);
-		int add = find(y);
+	private static class Node {
+		private int depth = 1;
+		private Node parent = null;
 		
-		if (ele == add) {
-			
+		private long max;
+		
+		public Node(long number) {
+			max = number;
+		}
+		private Node root() {
+			if (parent == null) return this;
+			return parent.root();
 		}
 		
+		public long max() {
+			return root().max;
+		}
 		
+		private boolean isConnected(Node o) {
+			return root() == o.root();
+		}
 		
-		return true;
+		public void merge(Node o) {
+			if (isConnected(o)) return;
+			
+			Node root1 = root();
+			Node root2 = o.root();
+			
+			if (root1.depth > root2.depth) {
+				root2.parent = root1;
+			} else if (root1.depth < root2.depth) {
+				root1.parent = root2;
+			} else {
+				root2.parent = root1;
+				root1.depth += 1;
+			}
+			root1.max = root2.max = Math.max(root1.max, root2.max);
+		}
 	}
 	
 	public long[] solution(long k, long[] room_number) {
-		Set<Integer> set = new HashSet<>();
+		List<Long> assigned = new ArrayList<>();
+		
+		// 유니온 파인드 수행 
+		
+		Map<Long, Node> nodes = new HashMap<>();
+		
+	for (long number : room_number) {
+		if (nodes.containsKey(number)) {
+			number = nodes.get(number).max() + 1;
+		}
+		
+		// number 방 실제 배정하기
+		
+		assigned.add(number);
+		
+		Node node = new Node(number);
+		nodes.put(number, node);
+		if (nodes.containsKey(number - 1)) {
+			node.merge(nodes.get(number - 1));
+		}
+		
+		if (nodes.containsKey(number + 1)) {
+			node.merge(nodes.get(number + 1));
+		}
 		
 		
+	}
 		
-        long[] answer = {};
-        return answer;
+        return assigned.stream().mapToLong(Long::longValue).toArray();
     }
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
