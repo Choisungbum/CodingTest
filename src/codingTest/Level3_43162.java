84f9af959f4c5d61b9e48f4e1c6dde7860071c6e
Choisungbum <42492672+Choisungbum@users.noreply.github.com>
2023-11-15 15:31:33 +0900

    finish : 43162
    43162

--- src/codingTest/Level3_43162.java
+++ src/codingTest/Level3_43162.java
@@ -1,55 +1,55 @@
 package codingTest;
 
 import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Set;
 import java.util.Stack;
 
 public class Level3_43162 {
-	 public int solution(int n, int[][] computers) {
-		 boolean[] isVisited = new boolean[computers.length];
-		 
-		 Stack<Integer> stack = new Stack<>();
-//		 for (int i = 0; i < ) {
-//			 
-//		 }
-		 stack.push(0);
-		 
-		 while(!stack.isEmpty()) {
-			 int index = stack.pop();
-			 System.out.println("index : " + index);
-			 if (isVisited[index]) continue;
-			 isVisited[index] = true;
-			 
-			 for (int j = 0; j < computers[0].length; j++) {
-				if (index == j) continue;
-				
-				if (computers[index][j] == 1) {
-					System.out.println("next index : " + j);
-					stack.push(j);
-				}
-			 }
-			 
-		 }
-		 isVisited.toString();
-		 int t = 0;
-		 int f = 0;
-		 for (boolean bool : isVisited) {
-			 if (bool == true) {
-				 t = 1;
-			 } else {
-				 f++;
-			 } 
-		 }
-		 
-		 System.out.println(Arrays.toString(isVisited));
-	        return t + f;
-	    }
+	private void visitAll(int computer, int[][] computers, boolean[] isVisited) {
+		Stack<Integer> stack = new Stack<Integer>();
+		stack.push(computer);
+		
+		while(!stack.isEmpty()) {
+			int c = stack.pop();
+			
+			if (isVisited[c]) continue;
+			isVisited[c] = true;
+			
+			for (int next = 0; next <  computers[c].length; next++) {
+				if(computers[c][next] == 0) continue;
+				stack.push(next);
+			}
+			
+		}
+		
+	}
+	
+ 	public int solution(int n, int[][] computers) {
+ 		boolean[] isVisited = new boolean[n];
+ 		int answer = 0;
+ 		
+ 		for (int i = 0; i < n; i++) {
+ 			if (isVisited[i]) continue;
+ 			visitAll(i, computers, isVisited);
+ 			answer++;
+ 		}
+ 		return answer++;
+    }
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		int n1 = 3;
 		int n2 = 4;
-		int[][] computers1 = new int[][] {{1, 1, 0}, {1, 1, 0}, {0, 0, 1}};
-		int[][] computers2 = new int[][] {{1, 1, 0}, {1, 1, 1}, {0, 1, 1}};
-		int[][] computers3 = new int[][] {{1, 1, 1, 0}, {1, 1, 1, 0}, {1, 1, 1, 0}, {0, 0, 0, 1}};
+		int[][] computers1 = new int[][] {{1, 1, 0}
+										, {1, 1, 0}
+										, {0, 0, 1}};
+		int[][] computers2 = new int[][] {{1, 1, 0}
+										, {1, 1, 1}
+										, {0, 1, 1}};
+		int[][] computers3 = new int[][] {{1, 0, 1, 0}
+										, {0, 1, 0, 1}
+										, {1, 0, 1, 0}
+										, {0, 1, 0, 1}};
 		Level3_43162 cls = new Level3_43162();
 		System.out.println(cls.solution(n2, computers3));
 	}
