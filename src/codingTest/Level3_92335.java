0b9732b80c34bbbb0e1a6d05a76e5cb4b8214bf3
Choisungbum <42492672+Choisungbum@users.noreply.github.com>
2023-12-22 17:57:13 +0900

    fix : k진수에서 소수 개수 구하기_92335
    k진수에서 소수 개수 구하기_92335

--- src/codingTest/Level3_92335.java
+++ src/codingTest/Level3_92335.java
@@ -1,7 +1,10 @@
 package codingTest;
 
 public class Level3_92335 {
-
+	public int solution(int n, int k) {
+        int answer = -1;
+        return answer;
+    }
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
